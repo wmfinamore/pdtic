2b920b78351c0627864ef2728bf2a6ac9f526eee
Willian Moreira Finamore <wmfinamore@hotmail.com>
2025-01-05 22:52:31 -0300

    Implementada classe PrincioDiretrizAdmin na app planos.

--- apps/planos/admin.py
+++ apps/planos/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import PrincipioDiretriz
+from .forms import PrincipioDiretrizForm
+from simple_history.admin import SimpleHistoryAdmin
 
-# Register your models here.
+
+@admin.register(PrincipioDiretriz)
+class PrincipioDiretrizAdmin(SimpleHistoryAdmin):
+    form = PrincipioDiretrizForm
+    model = PrincipioDiretriz
+    list_display = ['codigo', 'principio_diretriz', 'origem', 'criterio_priorizacao',]
+    search_fields = ['codigo', 'principio_diretriz', 'origem', ]
+    list_filter = ['origem', 'criterio_priorizacao',]
+    fieldsets = (
+        (None, {
+            'fields': (
+                ('codigo', 'principio_diretriz'),
+                ('origem', 'criterio_priorizacao'),
+            )
+        }),
+    )
