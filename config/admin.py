68fd1fc224a759fd5a5136db81fd16355833ab4c
Willian Moreira Finamore <wmfinamore@hotmail.com>
2025-01-05 23:21:49 -0300

    Correção do header do django admin

--- config/admin.py
+++ config/admin.py
@@ -1,9 +1,8 @@
 from django.contrib.admin import AdminSite
 
 
-
 class PdticAdminSite(AdminSite):
-    site_header = 'Gestão do Plano Diretor de TECNOLOGIA DA INFORMAÇÃO E COMUNICAÇÃO'
+    site_header = 'Gestão do Plano Diretor de Tecnologia da Informação e Comunicação'
     site_title = 'PDTIC'
     index_title = 'Administração'
     site_url = '/pdtic'
